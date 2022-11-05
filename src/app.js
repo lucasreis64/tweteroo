f2f88d1e9284ddf5351e6a9b92854e5ae6e4efcf
Lucas <lucasreis64@gmail.com>
2022-11-05 14:55:23 -0300

    Post Tweet

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express, { json } from "express" 
 import cors from "cors"
-import { users } from "./aux/constants.js"
+import { tweets, users } from "./aux/constants.js"
 
 const app = express()
 app.use(cors())
@@ -10,6 +10,18 @@ app.post("/sign-up", (req,res)=>{
     const signUp = req.body
     users.push(signUp)
     res.send('OK')
+})
+
+app.post("/tweets", (req,res)=>{
+    const tweet = req.body
+    tweets.push(tweet)
+    users.forEach((u,idx)=>{
+        if(u.username===tweet.username){
+            tweet.avatar=u.avatar
+        }
+    })
+
+    res.send(tweets)
 })
 
 app.listen(5000, ()=>{
--- src/aux/constants.js
+++ src/aux/constants.js
@@ -1,3 +1,13 @@
-const users = []
+const users = [{
+	username: 'bobesponja', 
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+}]
+const tweets = [[
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	}
+]]
 
-export {users}
+export {users, tweets}
